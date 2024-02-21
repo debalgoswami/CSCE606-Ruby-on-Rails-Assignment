d7d8b4eaeecb22ec04ca721c874a84870d88634a
debalgoswami <debal.goswami00@gmail.com>
2024-02-21 12:49:18 -0600

    Ruby on Rails app

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+    {:title => 'My Neighbor Totoro', :rating => 'G',
+        :release_date => '16-Apr-1988'},
+    {:title => 'Green Book', :rating => 'PG-13',
+        :release_date => '16-Nov-2018'},
+    {:title => 'Parasite', :rating => 'R',
+        :release_date => '30-May-2019'},
+    {:title => 'Nomadland', :rating => 'R',
+        :release_date => '19-Feb-2021'},
+    {:title => 'CODA', :rating => 'PG-13',
+        :release_date => '13-Aug-2021'},
+    {:title => 'Godfather 1', :rating => 'PG-13',
+        :release_date => '13-Sep-1972'},
+    {:title => 'Godfather 2', :rating => 'PG-13',
+        :release_date => '13-Sep-1974'},
+    {:title => 'Godfather 3 : Michael Corleone', :rating => 'PG-13',
+        :release_date => '13-Sep-2003'},
+    {:title => 'Interstellar', :rating => 'PG-13',
+        :release_date => '13-Aug-2016'}
+
+    ]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
